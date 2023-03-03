36ffad9e94b20a964989a8bfeb083972bad9edf4
Sc4ndium90 <b1jam1riot@gmail.com>
2023-03-03 22:36:46 +0100

    Create index.php with navbar including libs from Bootstrap, Popper and jQuery + a little script that add active class to the good item in the navbar, so you see on what page you are

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Styles -->
+    <!-- Bootstrap 5.2 -->    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <title>VirtuPanel - Home</title>
+</head>
+<body>
+
+    <!-- Side Navbar -->
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/components/navbar/navbar.html" ?>
+
+
+</body>
+
+    <!-- Scripts -->
+    <!-- Bootstrap 5.2 -->    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!-- Popper -->           <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <!-- jQuery -->           <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('a[href="' + this.location.pathname + '"]').addClass('active');
+
+            console.log(this.location.pathname)
+        });
+    </script>
+
+</html>
