4aecdbcfccdec6b9628be6a9c05552cff2755075
Sc4ndium90 <b1jam1riot@gmail.com>
2023-03-04 16:53:58 +0100

    Import new scripts and stylesheets

--- processes.php
+++ processes.php
@@ -8,6 +8,10 @@
 
     <!-- Styles -->
     <!-- Bootstrap 5.2 -->    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- DataTables BS5 -->   <link href="https://cdn.datatables.net/v/bs5/dt-1.13.3/datatables.min.css" rel="stylesheet" />
+    <!-- DataTables -->       <link href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css" rel="stylesheet" />
+
+    <style>.toolbar {float: left;}</style>
 
     <title>VirtuPanel - Processes</title>
 </head>
@@ -23,7 +27,10 @@
     <!-- Bootstrap 5.2 -->    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <!-- Popper -->           <script src="https://unpkg.com/@popperjs/core@2"></script>
     <!-- jQuery -->           <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+    <!-- DataTables -->       <script src="https://cdn.datatables.net/v/bs5/dt-1.13.3/datatables.min.js"></script>
+    <!-- SweetAlert -->       <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+<script src="js/processes.js"></script>
     <script>
         $(document).ready(function() {
             $('a[href="' + this.location.pathname + '"]').addClass('active');
