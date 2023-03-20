7516d0ae0fd951d87b788f9204273a7cfacc3d81
Sc4ndium90 <b1jam1riot@gmail.com>
2023-03-20 13:16:19 +0100

    Add comments

--- js/processes.js
+++ js/processes.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // Init the table with DataTable
     const processesTable = $('#processesTable')
 
     processesTable.DataTable({
@@ -13,18 +14,22 @@ $(document).ready(function () {
         paging: false
     })
 
+    // Set a custom toolbar for the table
     $('div.toolbar').html(
         '<button class="btn btn-primary me-2" id="refreshBtn"><svg class="bi pe-none" width="16" height="16"><use xlink:href="#refresh"></use></svg></button>'+
         '<button class="btn btn-danger" id="killProcessBtn">Kill a process</button>'
 
     );
 
+    // On click, refresh the table
     $('#refreshBtn').click(function () {
         processesTable.DataTable().ajax.reload();
     })
 
+    // Kill a process
     $('#killProcessBtn').click(function () {
 
+        // Fire a modal with Sweet Alerts
         Swal.fire({
             text: 'Enter the PID of the process you want to kill',
             input: 'text',
@@ -34,13 +39,13 @@ $(document).ready(function () {
             showCancelButton: false,
             confirmButtonText: 'Kill it!',
             showLoaderOnConfirm: true,
-            preConfirm: (input) => {
+            preConfirm: (input) => { // Send the data to the php file
                 return $.post('./ajax/killProcess.php', { id: input }).then(function (res) { return res })
             },
-            allowOutsideClick: () => !Swal.isLoading()
+            allowOutsideClick: () => !Swal.isLoading() // Allow user to click outside the modal to close it
         }).then((result) => {
             if (result.isConfirmed) {
-                if (result.value == '') {
+                if (result.value == '') { // If nothing, the command is a success, show the success modal
                     let timerInterval
                     Swal.fire({
                         title: "Process killed", icon: "success",
@@ -48,9 +53,9 @@ $(document).ready(function () {
                         didOpen: () => { Swal.showLoading() },
                         willClose: () => { clearInterval(timerInterval) }
                     }).then(() => {
-                        processesTable.DataTable().ajax.reload();
+                        processesTable.DataTable().ajax.reload(); // Refresh the table
                     })
-                } else {
+                } else { // Else show the error modal with the error
                     let timerInterval
                     Swal.fire({
                         title: "Error", icon: "error",
@@ -59,7 +64,7 @@ $(document).ready(function () {
                         didOpen: () => { Swal.showLoading() },
                         willClose: () => { clearInterval(timerInterval) }
                     }).then(() => {
-                        processesTable.DataTable().ajax.reload();
+                        processesTable.DataTable().ajax.reload(); // Refresh the table
                     })
                 }
             }
