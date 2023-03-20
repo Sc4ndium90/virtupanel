28af83b6c1a05dbdbfe30392d99fb93e208fa178
Sc4ndium90 <b1jam1riot@gmail.com>
2023-03-20 19:31:54 +0100

    Add comments

--- ajax/createFile.php
+++ ajax/createFile.php
@@ -12,11 +12,13 @@ try {
     $regex = "/^[a-zA-Z.](?!.*?[_-]{2})[a-zA-Z0-9_-]{1,58}[a-zA-Z0-9]$/";
 
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $filename = $_POST['name'];
-        $path = $_POST['path'];
-        if (!is_dir($path)) { echo "Invalid location"; return; }
+        $filename = $_POST['name']; // File name
+        $path = $_POST['path']; // Actual path
+        if (!is_dir($path)) { echo "Invalid location"; return; } // If path is invalid
+        // Check if the filed name is safe
         if (!preg_match_all($regex, $filename)) { echo "Invalid directory name (must be only letters (no accents), numbers, _ or -, 3 characters min and 60 max)"; return; }
 
+        // Create the file
         $execString = "sudo touch " . $path.$filename;
         $output = exec($execString);
 
