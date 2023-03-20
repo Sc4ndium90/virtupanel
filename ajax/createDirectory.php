555827a29c822eb41c1cc62c3839f7ddc5641d00
Sc4ndium90 <b1jam1riot@gmail.com>
2023-03-20 19:31:50 +0100

    Add comments

--- ajax/createDirectory.php
+++ ajax/createDirectory.php
@@ -12,11 +12,13 @@ try {
     $regex = "/^[a-zA-Z.](?!.*?[_-]{2})[a-zA-Z0-9_-]{1,30}[a-zA-Z0-9]$/";
 
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $dirname = $_POST['name'];
-        $path = $_POST['path'];
-        if (!is_dir($path)) { echo "Invalid location"; return; }
+        $dirname = $_POST['name']; // Directory name
+        $path = $_POST['path']; // Actual path
+        if (!is_dir($path)) { echo "Invalid location"; return; } // If the path is not a dir
+        // Check if the name is safe
         if (!preg_match_all($regex, $dirname)) { echo "Invalid directory name (must be only letters (no accents), numbers, _ or -, 3 characters min and 32 max)"; return; }
 
+        // Create the directory
         $execString = "sudo mkdir " . $path.$dirname;
         $output = exec($execString);
 
