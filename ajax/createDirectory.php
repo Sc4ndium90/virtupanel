3c548c11aa46712acdff6769878b410f6734646e
Sc4ndium90 <b1jam1riot@gmail.com>
2023-03-20 13:19:32 +0100

    Missing info in the comment about the regex

--- ajax/createDirectory.php
+++ ajax/createDirectory.php
@@ -6,7 +6,7 @@ try {
      * This regex allow only that :
      * - Start with a letter (not case-sensitive) or a dot
      * - End with a letter (not case-sensitive) or a number
-     * - Can not contain more than 2 _ or -
+     * - Can not contain more than 2 _ or - in a row
      * - 3 min and 32 max
      */
     $regex = "/^[a-zA-Z.](?!.*?[_-]{2})[a-zA-Z0-9_-]{1,30}[a-zA-Z0-9]$/";
